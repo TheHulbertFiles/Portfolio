d0ec84305055a656b5a45e52ef3f2f58b83ccf9f
Eoghan <eoghan@hulbert.info>
2020-10-09 10:51:40 -0400

    Modified Blog

--- Blog/models.py
+++ Blog/models.py
@@ -6,22 +6,22 @@ from ckeditor.fields import RichTextField
 from Global.models import Skills, Tags
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=200)
-    post_pub_date = models.DateTimeField()
-    post_author = models.CharField(max_length=200)
-    post_image = models.ImageField(upload_to='images/')
-    post_body = RichTextField()
-    post_mod_date = models.DateTimeField()
-    post_tags = models.ManyToManyField(Skills)
+    post_Title = models.CharField(max_length=200)
+    post_Pub_Date = models.DateTimeField()
+    post_Author = models.CharField(max_length=200)
+    post_Image = models.ImageField(upload_to='images/')
+    post_Body = RichTextField()
+    post_Mod_Date = models.DateTimeField()
+    post_tags = models.ManyToManyField(Tags)
 
     def __str__(self):
-        return self.post_title
+        return self.post_Title
 
     def summary(self):
-        return self.post_body[:200]
+        return self.post_Body[:200]
 
     def pub_date_mod(self):
-        return self.post_pub_date.strftime('%B %e, %Y')
+        return self.post_Pub_Date.strftime('%B %e, %Y')
 
     def mod_date_f(self):
-        return self.post_mod_date.strftime('%m/%d/%Y %H:%M:%S')
+        return self.post_Mod_Date.strftime('%m/%d/%Y %H:%M:%S')
