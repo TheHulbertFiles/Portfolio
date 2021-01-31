4baf571e1a3ee267bf770f995c5ed1dd5707b94f
Eoghan <eoghan@hulbert.info>
2021-01-30 23:15:24 -0800

    Updated settings.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -15,11 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-
 # Application definition
 
 INSTALLED_APPS = [
